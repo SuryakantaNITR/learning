5ba05d16e1bae4d38abf76433f54bf92232083e8
Suryakanta Pradhan <suryakanta.pradhan@innoviti.com>
2017-09-13 11:19:08 +0530

    Binomial coeffiecients calculation for given n and k

--- src/main/java/com/learning/paradigms/dp/BinomialCoefficient.java
+++ src/main/java/com/learning/paradigms/dp/BinomialCoefficient.java
@@ -2,4 +2,22 @@ package com.learning.paradigms.dp;
 
 public class BinomialCoefficient {
 
+	public static int calculateCoefficient(int n, int k) {		
+		int[][] cache = new int[n+1][k+1];
+		
+		for(int i=0;i<=n;i++) {
+			for(int j=0;j<=Math.min(i, k);j++) {
+				if(i==0 || j==0)
+					cache[i][j]=1;
+				else
+					cache[i][j] = cache[i-1][j-1] + cache[i-1][j];
+			}
+				
+		}
+		return cache[n][k];
+	}
+	public static void main(String[] args) {
+		int n = 5, k = 2;
+		System.out.println(calculateCoefficient(n, k));
+	}
 }
